d4ab15bd8925796f0d8a91e554ffb1b9e6b6b057
crishard <crislantorres9@gmail.com>
2024-11-19 19:11:08 -0300

    feature: criando base de dados incial do cardapio

---
+++ src/data/data.ts
@@ -0,0 +1,58 @@
+import ICradInterface from "../interfaces/ICardInterface";
+
+export const cardapioBase: ICradInterface[] = [
+    {
+        id: "1",
+        nome: "Pizza Frango com Bacon",
+        ingredientes:
+            "Farinha de trigo, Fermento, Água, Sal, Vinagre, Gordura, Molho de tomate, Queijo, Tomate, Orégano",
+
+        image: "https://i.pinimg.com/736x/2d/6b/7d/2d6b7df948d14f41e9a1a48e5b37e9c5.jpg",
+        valor: 50
+    },
+    {
+        id: "2",
+        nome: "Pizza Carne de sol",
+        ingredientes:
+            "Farinha de trigo, Fermento, Água, Sal, Vinagre, Gordura, Molho de tomate, Queijo, Tomate, Orégano",
+
+
+        image: "https://i.pinimg.com/736x/2d/6b/7d/2d6b7df948d14f41e9a1a48e5b37e9c5.jpg",
+        valor: 50
+    },
+    {
+        id: "3",
+        nome: "Pizza de Calabresa",
+        ingredientes:
+            "Farinha de trigo, Fermento, Água, Sal, Vinagre, Gordura, Molho de tomate, Queijo, Tomate, Orégano",
+        image: "https://i.pinimg.com/736x/2d/6b/7d/2d6b7df948d14f41e9a1a48e5b37e9c5.jpg",
+        valor: 50
+    },
+    {
+        id: "4",
+        nome: "Pizza de Carne Moída",
+        ingredientes:
+            "Farinha de trigo, Fermento, Água, Sal, Vinagre, Gordura, Molho de tomate, Queijo, Tomate, Orégano",
+
+        image: "https://i.pinimg.com/736x/2d/6b/7d/2d6b7df948d14f41e9a1a48e5b37e9c5.jpg",
+        valor: 50
+    },
+    {
+        id: "5",
+        nome: "Pizza Quatro queijos",
+        ingredientes:
+            "Farinha de trigo, Fermento, Água, Sal, Vinagre, Gordura, Molho de tomate, Queijo, Tomate, Orégano",
+
+        image: "https://i.pinimg.com/736x/2d/6b/7d/2d6b7df948d14f41e9a1a48e5b37e9c5.jpg",
+        valor: 50
+    },
+    {
+        id: "6",
+        nome: "Pizza de Camarão",
+        ingredientes:
+            "Farinha de trigo, Fermento, Água, Sal, Vinagre, Gordura, Molho de tomate, Queijo, Tomate, Orégano",
+
+        image: "https://i.pinimg.com/736x/2d/6b/7d/2d6b7df948d14f41e9a1a48e5b37e9c5.jpg",
+        valor: 50
+    },
+]
