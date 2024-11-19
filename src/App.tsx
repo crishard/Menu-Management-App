5b56d9b9fa8e371b32f510f705060d8049984c75
crishard <crislantorres9@gmail.com>
2024-11-19 19:26:24 -0300

    feature: chamando componente home

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
+import { Home } from "./pages/Home"
 
 function App() {
   return (
     <body  id="menu-content" className="h-svh bg-gray-200 text-gray-800">
-     
+      <Home />
     </body>
   )
 }
